dde41507bfc2afa8dd8e7253ebc6469082f8213d
hyunji <jhyj3768@gmail.com>
2022-07-01 14:44:43 +0900

    sqlite 연결

--- src/main/index.js
+++ src/main/index.js
@@ -2,20 +2,22 @@ import React,{useState, useEffect} from 'react';
 import './index.scss';
 import axios from 'axios';
 import ProductList from '../components/ProductList';
+import MainProduct from './MainProduct';
 
 const MainPage = (props) => {
-    // const [products, setProducts] = useState([]);
-    // useEffect(()=>{
-    //     axios.get("http://localhost:3000/products")
-    //     .then(result=>{
-    //         const products = result.data.products; //객체의 키로 접근
-    //         console.log(result.data.products);
-    //         setProducts(products);
-    //         console.log(products);
-    //     }).catch(e=>{
-    //         console.log(e);
-    //     })
-    // },[])
+    const [products, setProducts] = useState([]);
+    useEffect(()=>{
+        axios.get("http://localhost:3000/products")
+        .then(result=>{
+            const products = result.data //객체의 키로 접근
+            // console.log(result.data.products);
+            setProducts(products);
+            console.log(products);
+        }).catch(e=>{
+            console.log(e);
+        })
+    },[])
+    if(products===[]) return <div>로딩중입니다..</div>
     return (
         <div>
             
@@ -26,21 +28,11 @@ const MainPage = (props) => {
                 </div>
                 <div id="product-list" className='inner'>
                     <h2>멍멍이조명의 최신상품을 만나보세요🐾</h2>
-                    <ProductList/>
-                    {/* <div id="product-items">
-                        <div className='product-card'>
-                            <div className='product-img'>
-                                <img src="images/products/product1.jpg" alt=''/>
-                            </div>
-                            <div className='product-contents'>
-                                <span className='product-name'>제품명</span>
-                                <span className='product-price'>가격</span>
-                                <div className='product-seller'>
-                                    <img src="images/icons/avatar.png" alt=""/>김멍멍
-                                </div>
-                            </div>
-                        </div>
-                    </div> */}
+                    <div id="product-items">
+                        {products.map(product=><MainProduct key={product.id} product={product}/>
+                        )}
+                        
+                    </div>
                 </div>
             </div>
 
--- src/product/index.js
+++ src/product/index.js
@@ -1,31 +1,51 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import "./product.scss";
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const ProductPage = () => {
+    const [product, setProduct] = useState(null);
+
+    //useParams()를 실행시키면 파라미터 값을 가지고 있는 객체를 반환해줌.
+    const { id } = useParams();
+    console.log(id);
+    //useEffect 사용할때 두번째 인자에 빈배열을 넣어주면 한번만 실행됨.
+    useEffect(()=>{
+        axios.get(`http://localhost:3000/product/${id}`)
+        .then(result=>{
+            const data = result.data;
+            console.log(data);
+            setProduct(data);
+        })
+        .catch(e=>{
+            console.log(e);
+        })
+    },[]);
+    if(!product) return <div>~로딩중~</div>;
     return (
         <div className='inner'>
             <div id="img-box">
-                <img src="images/products/product1.jpg" alt=''/>
+                <img src={product.imgsrc} alt=''/>
             </div>
             <div id="profile-box">
                 <ul>
                     <li>
                         <div>
-                            <img src="images/icons/avatar.png" alt=""/>
-                            <span>김멍멍</span>
+                            <img src="/images/icons/avatar.png" alt=""/>
+                            <span>{product.seller}</span>
                         </div>
                     </li>
                     <li>
-                        제품명 : 새로운 조명
+                       상품명: {product.name}
                     </li>
                     <li>
-                        가격 : 50,000원
+                        가격 :{product.price}
                     </li>
                     <li>
-                        등록일 : 2022년 6월 29일
+                       등록일 : 2022. 06. 22
                     </li>
                     <li>
-                        상세설명 : 김멍멍씨가 픽한 아주 새로운 조명
+                       상품설명 : 아주 새로운 조명 
                     </li>
                 </ul>
             </div>
